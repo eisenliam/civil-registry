3da99fc7a17e5bfdcde19af9bdb94ab555173ce6
CPE3C-pahati-eisenliam <2023101848@ms.bulsu.edu.ph>
2026-05-11 14:52:11 +0800

    login UI

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export interface LabelProps
+  extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+
+export const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
+  ({ className, ...props }, ref) => (
+    <label
+      ref={ref}
+      className={`block text-sm font-medium text-gray-900 ${className || ''}`}
+      {...props}
+    />
+  )
+);
+
+Label.displayName = 'Label';
