fc504d37b5d76e0a3eeb6e2ed48e3b5c9d426564
sulas0k <irunadummyy2@gmail.com>
2026-05-11 09:08:00 +0800

    Initial commit

---
+++ src/types/database.ts
@@ -0,0 +1,19 @@
+export type Database = {
+  public: {
+    Tables: {
+      [_ in never]: never;
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+};
---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#EA580C', // Orange
+        secondary: '#7F1D1D', // Maroon
+        white: '#FFFFFF',
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
